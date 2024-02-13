8cfa095c59f34f41bba860b7d2345e828591e352
sonal kumar mandal <152254912+SONALkumarmandal@users.noreply.github.com>
2024-02-13 07:23:00 +0000

    js1

---
+++ js_basics/var_let_const.js
@@ -0,0 +1,14 @@
+const rollNumber=27;
+let city="Bokaro";
+var name="sonal";
+console.log(rollNumber);
+console.log(city);
+console.log(name);
+console.table([rollNumber,city,name]);
+
+//note--> DONT USE VAR BCZ OF ISSUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+
+let accountState;
+console.log(accountState);
+
+// note--> here it is UNDEFINED BCZ we havent defined the variable
